7ded00b78a7d68c0a02d1ef98d1781de46d9ebd0
Adityalodhi <adityalodhi2003@gmail.com>
2025-01-06 03:05:41 +0530

    chages

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,6 @@ const Header = () => {
   return (
     <header className="App-header">
       <h1>Boys Hostel Presents</h1>
-      {/* <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/sport/1">Football</Link></li>
-          <li><Link to="/sport/2">Basketball</Link></li>
-          <li><Link to="/sport/3">Cricket</Link></li>
-          <li><Link to="/sport/4">Volleyball</Link></li>
-        </ul>
-      </nav> */}
     </header>
   );
 };
